e1192b19f1c38183c95d192149a19fa67b4416df
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-07-12 10:01:49 -0500

    fixies from review

--- dpnp/backend/examples/example_bs_sycl.cpp
+++ dpnp/backend/examples/example_bs_sycl.cpp
@@ -1,35 +1,29 @@
 #include <CL/sycl.hpp>
 #include <iostream>
 
-cl::sycl::event divide_sycl(double* price,
-                            double* strike,
+cl::sycl::event divide_sycl(cl::sycl::queue q,
                             const size_t size,
-                            double* p_div_s,
-                            cl::sycl::queue q)
+                            double* price,
+                            double* strike,
+                            double* p_div_s)
 {
-    double* P = sycl::malloc_shared<double>(size, q);
-    double* S = sycl::malloc_shared<double>(size, q);
-
-    P = price;
-    S = strike;
+    double* P = price;
+    double* S = strike;
 
     cl::sycl::event event = q.submit([&](sycl::handler &cgh) {
 	    cgh.parallel_for(size, [=](sycl::item<1> i) { p_div_s[i] = P[i] / S[i];});
     });
 
-    cl::sycl::free(P, q);
-    cl::sycl::free(S, q);
-
     return event;  
 }
 
-cl::sycl::event log_sycl(const size_t size,
+cl::sycl::event log_sycl(cl::sycl::queue q,
+                         const size_t size,
                          double* p_div_s,
-                         double* log_,
-                         cl::sycl::queue q)
+                         double* a)
 {
     cl::sycl::event event = q.submit([&](sycl::handler &cgh) {
-	    cgh.parallel_for(size, [=](sycl::item<1> i) { log_[i] = cl::sycl::log(p_div_s[i]);});
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { a[i] = cl::sycl::log(p_div_s[i]);});
     });
 
     return event;
@@ -44,26 +38,26 @@ void black_scholes(double* price,
                    cl::sycl::queue q)
 {
     // ------------ dividing ----------
-    double(*p_div_s) = sycl::malloc_shared<double>(size, q);
+    double* p_div_s = sycl::malloc_shared<double>(size, q);
 
     cl::sycl::event event;
-    event = divide_sycl(price, strike, size, p_div_s, q);
+    event = divide_sycl(q, size, price, strike, p_div_s);
     event.wait();
 
     for (int i = 0; i < size; ++i)
         std::cout << price[i] << " / " << strike[i] << " = " << p_div_s[i] << "\n";
 
     // ------------ log ----------
-    double(*log_) = sycl::malloc_shared<double>(size, q);
+    double* a = sycl::malloc_shared<double>(size, q);
 
-    event = log_sycl(size, p_div_s, log_, q);
+    event = log_sycl(q, size, p_div_s, a);
     event.wait();
 
     for (int i = 0; i < size; ++i)
-        std::cout << log_[i] << "\n";
+        std::cout << a[i] << "\n";
     
     cl::sycl::free(p_div_s, q);
-    cl::sycl::free(log_, q);
+    cl::sycl::free(a, q);
 }
 
 
@@ -80,8 +74,8 @@ int main() {
     cl::sycl::queue q{sycl::gpu_selector{}};
 
     //------ example -------
-    double(*price) = sycl::malloc_shared<double>(SIZE, q);
-    double(*strike) = sycl::malloc_shared<double>(SIZE, q);
+    double *price = sycl::malloc_shared<double>(SIZE, q);
+    double *strike = sycl::malloc_shared<double>(SIZE, q);
 
     for (int i = 0; i < SIZE; i++)
     {
@@ -92,6 +86,5 @@ int main() {
     black_scholes(price, strike, RISK_FREE, VOLATILITY, SIZE, q);
 
     cl::sycl::free(price, q);
-    
     cl::sycl::free(strike, q);
 }
