742b54564170e5f205313d8787725898423aac09
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-07-15 10:26:31 -0500

    Update example_bs_sycl.cpp

--- dpnp/backend/examples/example_bs_sycl.cpp
+++ dpnp/backend/examples/example_bs_sycl.cpp
@@ -2,59 +2,135 @@
 #include <iostream>
 #include <chrono>
 
-cl::sycl::event divide_sycl(cl::sycl::queue q,
-                            std::vector<cl::sycl::event> &dep_e,
-                            const size_t size,
-                            double* a,
-                            double* b,
-                            double* y)
+const int reprtitions = 15;
+
+sycl::event divide_array_by_array(sycl::queue q,
+                                      std::vector<sycl::event> &deps,
+                                      const size_t size,
+                                      double* a,
+                                      double* b,
+                                      double* y)
 {
-    cl::sycl::event event = q.submit([&](sycl::handler &cgh) {
-        cgh.depends_on(dep_e);
-	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = a[i] / b[i];});
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = a[i] / b[i];}); 
     });
 
     return event;  
 }
 
-cl::sycl::event log_sycl(cl::sycl::queue q,
-                         std::vector<cl::sycl::event> &dep_e,
+sycl::event divide_array_scalar(sycl::queue q,
+                                    std::vector<sycl::event> &deps,
+                                    const size_t size,
+                                    double a,
+                                    double* b,
+                                    double* y)
+{
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = a / b[i];});
+    });
+
+    return event;  
+}
+
+sycl::event log_sycl(sycl::queue q,
+                         std::vector<sycl::event> &deps,
                          const size_t size,
                          double* a,
                          double* y)
 {
-    cl::sycl::event event = q.submit([&](sycl::handler &cgh) {
-        cgh.depends_on(dep_e);
-	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = cl::sycl::log(a[i]);});
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = sycl::log(a[i]);});
     });
 
     return event;
 }
 
-cl::sycl::event multiply_array_by_scalar(cl::sycl::queue q,
-                                         std::vector<cl::sycl::event> &dep_e,
+sycl::event multiply_array_by_scalar(sycl::queue q,
+                                         std::vector<sycl::event> &deps,
                                          const size_t size,
                                          double* a,
                                          double b,
                                          double* y)
 {
-    cl::sycl::event event = q.submit([&](sycl::handler &cgh) {
-        cgh.depends_on(dep_e);
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
 	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = a[i] * b;});
     });
 
     return event;
 }
 
-cl::sycl::event sqrt(cl::sycl::queue q,
-                     std::vector<cl::sycl::event> &dep_e,
+sycl::event multiply_arrays(sycl::queue q,
+                                std::vector<sycl::event> &deps,
+                                const size_t size,
+                                double* a,
+                                double* b,
+                                double* y)
+{
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = a[i] * b[i];});
+    });
+
+    return event;
+}
+
+sycl::event sqrt(sycl::queue q,
+                     std::vector<sycl::event> &deps,
                      const size_t size,
                      double* a,
                      double* y)
 {
-    cl::sycl::event event = q.submit([&](sycl::handler &cgh) {
-        cgh.depends_on(dep_e);
-	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = cl::sycl::sqrt(a[i]);});
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = sycl::sqrt(a[i]);});
+    });
+
+    return event;
+}
+
+sycl::event subtract(sycl::queue q,
+                            std::vector<sycl::event> &deps,
+                            const size_t size,
+                            double* a,
+                            double* b,
+                            double* y)
+{
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = a[i] - b[i];});
+    });
+
+    return event;
+}
+
+sycl::event add(sycl::queue q,
+                    std::vector<sycl::event> &deps,
+                    const size_t size,
+                    double* a,
+                    double* b,
+                    double* y)
+{
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = a[i] + b[i];});
+    });
+
+    return event;
+}
+
+sycl::event erf(sycl::queue q,
+                         std::vector<sycl::event> &deps,
+                         const size_t size,
+                         double* a,
+                         double* y)
+{
+    sycl::event event = q.submit([&](sycl::handler &cgh) {
+        cgh.depends_on(deps);
+	    cgh.parallel_for(size, [=](sycl::item<1> i) { y[i] = sycl::erf(a[i]);});
     });
 
     return event;
@@ -66,92 +142,168 @@ void black_scholes(double* price,
                    const double rate,
                    const double vol,
                    const size_t size,
-                   cl::sycl::queue q,
-                   bool is_sync = false)
+                   sycl::queue q,
+                   bool sync = false)
 {
-    double mr = - rate;
-
+    double mr = -rate;
     double vol_vol_two = vol * vol * 2;
 
-    double quarter = 0.25;
-
-    double one = 1.;
-
-    double half = 0.5;
-
     // ------------ p_div_s = price / strike ----------
-    std::vector<cl::sycl::event> dep_events_p_div_s;
+    std::vector<sycl::event> depsvents_p_div_s;
     double* p_div_s = sycl::malloc_shared<double>(size, q);
-    cl::sycl::event e_div = divide_sycl(q, dep_events_p_div_s, size, price, strike, p_div_s);
+    sycl::event e_div = divide_array_by_array(q, depsvents_p_div_s, size, price, strike, p_div_s);
 
-    if (is_sync)
+    if (sync)
         e_div.wait();
 
     // ------------ a = log(p_div_s) ------------
-    std::vector<cl::sycl::event> dep_events_a;
-    if (!is_sync) 
-        dep_events_a.push_back(e_div);
+    std::vector<sycl::event> depsvents_a;
+    if (!sync) 
+        depsvents_a.push_back(e_div);
     double* a = sycl::malloc_shared<double>(size, q);
-    cl::sycl::event e_log = log_sycl(q, dep_events_a, size, p_div_s, a);
+    sycl::event e_a = log_sycl(q, depsvents_a, size, p_div_s, a);
 
-    if (is_sync)
-        e_log.wait();
+    if (sync)
+        e_a.wait();
 
     // ------------ b = t * mr ------------
-    std::vector<cl::sycl::event> dep_events_b;
-    double* b = cl::sycl::malloc_shared<double>(size, q);
-    cl::sycl::event e_b = multiply_array_by_scalar(q, dep_events_b, size, t, mr, b);
+    std::vector<sycl::event> depsvents_b;
+    double* b = sycl::malloc_shared<double>(size, q);
+    sycl::event e_b = multiply_array_by_scalar(q, depsvents_b, size, t, mr, b);
 
-    if (is_sync)
+    if (sync)
         e_b.wait();
 
     //------------ z = t * vol_vol_two ------------
-    std::vector<cl::sycl::event> dep_events_z;
-    double* z = cl::sycl::malloc_shared<double>(size, q);
-    cl::sycl::event e_z = multiply_array_by_scalar(q, dep_events_z, size, t, vol_vol_two, z);
-    if (is_sync)
+    std::vector<sycl::event> depsvents_z;
+    double* z = sycl::malloc_shared<double>(size, q);
+    sycl::event e_z = multiply_array_by_scalar(q, depsvents_z, size, t, vol_vol_two, z);
+    if (sync)
         e_z.wait();
 
     //------------ c = quartnes * z ------------
-    std::vector<cl::sycl::event> dep_events_c;
-    if (!is_sync)
-        dep_events_c.push_back(e_z);
-    double* c = cl::sycl::malloc_shared<double>(size, q);
-    cl::sycl::event e_c = multiply_array_by_scalar(q, dep_events_c, size, z, quarter, c);
-    if (is_sync)
+    std::vector<sycl::event> depsvents_c;
+    if (!sync)
+        depsvents_c.push_back(e_z);
+    double* c = sycl::malloc_shared<double>(size, q);
+    sycl::event e_c = multiply_array_by_scalar(q, depsvents_c, size, z, 0.25, c);
+    if (sync)
         e_c.wait();
 
     //------------ sqrt(z) ------------
-    std::vector<cl::sycl::event> dep_events_sqrt_z;
-    if (!is_sync)
-        dep_events_sqrt_z.push_back(e_z);
-    double* sqrt_z = cl::sycl::malloc_shared<double>(size, q);
-    cl::sycl::event e_sqrt_z = sqrt(q, dep_events_sqrt_z, size, z, sqrt_z);
-    if (is_sync)
+    std::vector<sycl::event> depsvents_sqrt_z;
+    if (!sync)
+        depsvents_sqrt_z.push_back(e_z);
+    double* sqrt_z = sycl::malloc_shared<double>(size, q);
+    sycl::event e_sqrt_z = sqrt(q, depsvents_sqrt_z, size, z, sqrt_z);
+    if (sync)
         e_sqrt_z.wait();
 
     //------------ y = ones / sqrt(z) ------------
-    // std::vector<cl::sycl::event> dep_events_y;
-    // if (!is_sync)
-    //     dep_events_sqrt_z.push_back(e_sqrt_z);
-    // double* y = cl::sycl::malloc_shared<double>(size, q);
-    // cl::sycl::event e_y = divide_sycl(q, dep_events_y, size, &one, sqrt_z, y);
-    // if (is_sync)
-    //     e_y.wait();
+    std::vector<sycl::event> depsvents_y;
+    if (!sync)
+        depsvents_y.push_back(e_sqrt_z);
+    double* y = sycl::malloc_shared<double>(size, q);
+    sycl::event e_y = divide_array_scalar(q, depsvents_y, size, 1, sqrt_z, y);
+    if (sync)
+        e_y.wait();
+
+    //------------ a_sub_b = a - b ------------
+    std::vector<sycl::event> depsvents_a_sub_b;
+    if (!sync) 
+    {
+        depsvents_a_sub_b.insert(depsvents_a_sub_b.end(), {e_a, e_b});
+    }
+    double* a_sub_b = sycl::malloc_shared<double>(size, q);
+    sycl::event e_a_sub_b = subtract(q, depsvents_a_sub_b, size, a, b, a_sub_b);
+    if (sync)
+        e_a_sub_b.wait();
+
+    //------------ a_sub_b_add_c = a_sub_b + c ------------
+    std::vector<sycl::event> depsvents_a_sub_b_add_c;
+    if (!sync) 
+    {
+        depsvents_a_sub_b_add_c.insert(depsvents_a_sub_b_add_c.end(), {e_c, e_a_sub_b});
+    }
+    double* a_sub_b_add_c = sycl::malloc_shared<double>(size, q);
+    sycl::event e_a_sub_b_add_c = add(q, depsvents_a_sub_b_add_c, size, a_sub_b, c, a_sub_b_add_c);
+    if (sync)
+        e_a_sub_b_add_c.wait();
+
+    //------------ w1 = a_sub_b_add_c * y ------------
+    std::vector<sycl::event> depsvents_w1;
+    if (!sync) {
+        depsvents_a_sub_b_add_c.insert(depsvents_a_sub_b_add_c.end(), {e_a_sub_b_add_c, e_y});
+    }
+    double* w1 = sycl::malloc_shared<double>(size, q);
+    sycl::event e_w1 = multiply_arrays(q, depsvents_w1, size, a_sub_b, c, a_sub_b_add_c);
+    if (sync)
+        e_w1.wait();
+
+     //------------ a_sub_b_sub_c = a_sub_b - c ------------
+    std::vector<sycl::event> depsvents_a_sub_b_sub_c;
+    if (!sync) 
+    {
+        depsvents_a_sub_b_add_c.insert(depsvents_a_sub_b_add_c.end(), {e_c, e_a_sub_b});
+    }
+    double* a_sub_b_sub_c = sycl::malloc_shared<double>(size, q);
+    sycl::event e_a_sub_b_sub_c = subtract(q, depsvents_a_sub_b_sub_c, size, a_sub_b, c, a_sub_b_sub_c);
+    if (sync)
+        e_a_sub_b_sub_c.wait();
+
+    //------------ w2 = a_sub_b_sub_c * y ------------
+    std::vector<sycl::event> depsvents_w2;
+    if (!sync) {
+        depsvents_a_sub_b_add_c.insert(depsvents_a_sub_b_add_c.end(), {e_a_sub_b_sub_c, e_y});
+    }
+    double* w2 = sycl::malloc_shared<double>(size, q);
+    sycl::event e_w2 = multiply_arrays(q, depsvents_w2, size, a_sub_b, c, a_sub_b_sub_c);
+    if (sync)
+        e_w2.wait();
+
+    //------------ erf_w1 = erf(w1) ------------
+    std::vector<sycl::event> depsvents_erf_w1;
+    if (!sync) {
+        depsvents_a_sub_b_add_c.push_back(e_w1);
+    }
+    double* erf_w1 = sycl::malloc_shared<double>(size, q);
+    sycl::event e_erf_w1 = erf(q, depsvents_erf_w1, size, w1, erf_w1);
+    if (sync)
+        e_erf_w1.wait();
+
+    //------------ halfs_mul_erf_w1 = halfs * erf_w1 ------------
+    std::vector<sycl::event> depsvents_halfs_mul_erf_w1;
+    if (!sync) {
+        depsvents_a_sub_b_add_c.push_back(e_erf_w1);
+    }
+    double* halfs_mul_erf_w1 = sycl::malloc_shared<double>(size, q);
+    sycl::event e_halfs_mul_erf_w1 = multiply_array_by_scalar(q, depsvents_halfs_mul_erf_w1, size, erf_w1, 0.5, halfs_mul_erf_w1);
+    if (sync)
+        e_halfs_mul_erf_w1.wait();
     
-    cl::sycl::free(p_div_s, q);
-    cl::sycl::free(a, q);
-    cl::sycl::free(b, q);
-    cl::sycl::free(z, q);
-    cl::sycl::free(c, q);
-    cl::sycl::free(sqrt_z, q);
-    //cl::sycl::free(y, q);
+    sycl::free(p_div_s, q);
+    sycl::free(a, q);
+    sycl::free(b, q);
+    sycl::free(z, q);
+    sycl::free(c, q);
+    sycl::free(sqrt_z, q);
+    sycl::free(y, q);
+    sycl::free(a_sub_b, q);
+
+    // free memory
 }
 
-int median(std::vector<int> performance)
+double random(int a, int b) 
 {
-    std::sort(performance.begin(), performance.end());
-    return performance[performance.size()/2];
+    return (double) rand() / (double) RAND_MAX * (b - a) + b;
+}
+
+double median(std::vector<double> times)
+{
+    std::sort(times.begin(), times.end());
+    if (times.size() % 2)
+        return times[times.size()/2];
+    return times[times.size()/2 - 1] + times[times.size()/2] / 2;
 }
 
 int main() {
@@ -164,7 +316,7 @@ int main() {
     const double RISK_FREE = 0.1;
     const double VOLATILITY = 0.2;
     
-    cl::sycl::queue q{sycl::gpu_selector{}};
+    sycl::queue q{sycl::gpu_selector{}};
 
     double *price = sycl::malloc_shared<double>(SIZE, q);
     double *strike = sycl::malloc_shared<double>(SIZE, q);
@@ -172,38 +324,38 @@ int main() {
 
     for (int i = 0; i < SIZE; i++)
     {
-        price[i] = (double) rand() / (double) RAND_MAX * (PH - PL) + PL;
-        strike[i] = (double) rand() / (double) RAND_MAX * (SH - SL) + SL;
-        t[i] = (double) rand() / (double) RAND_MAX * (TH -TL) + TL;
+        price[i] = random(PL, PH); 
+        strike[i] = random(SL, SH);
+        t[i] =  random(TL, TH);
     }
     
     //------------ performanse mesure ------------
-    std::vector<int> async_performances;
-    for (int i = 0; i < 15; ++i) 
+    std::vector<double> async_times;
+    for (int i = 0; i < reprtitions; ++i) // reprtitions
     {
         auto t1 = std::chrono::high_resolution_clock::now();
 
         black_scholes(price, strike, t, RISK_FREE, VOLATILITY, SIZE, q);
 
         auto t2 = std::chrono::high_resolution_clock::now();
-        async_performances.push_back(std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count());
+        async_times.push_back(std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count());
     }
 
-    std::vector<int> sync_performances;
-    for (int i = 0; i < 15; ++i) 
+    std::vector<double> sync_times;
+    for (int i = 0; i < reprtitions; ++i) 
     {
         auto t1 = std::chrono::high_resolution_clock::now();
 
         black_scholes(price, strike, t, RISK_FREE, VOLATILITY, SIZE, q, true);
 
         auto t2 = std::chrono::high_resolution_clock::now();
-        sync_performances.push_back(std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count());
+        sync_times.push_back(std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count());
     }
 
-    std::cout << "Async performance: " << median(async_performances) << "\n";
-    std::cout << "Sync performance: " << median(sync_performances) << "\n";
+    std::cout << "Async time: " << median(async_times) << " ms.\n";
+    std::cout << "Sync time: " << median(sync_times) << " ms.\n";
 
-    cl::sycl::free(price, q);
-    cl::sycl::free(strike, q);
-    cl::sycl::free(t, q);
+    sycl::free(price, q);
+    sycl::free(strike, q);
+    sycl::free(t, q);
 }
