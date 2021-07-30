d174b864b72f597c4f11f07a047298ea17b4a614
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-07-30 03:48:15 -0500

    fix style

--- dpnp/backend/include/dpnp_iface.hpp
+++ dpnp/backend/include/dpnp_iface.hpp
@@ -963,17 +963,17 @@ INP_DLLEXPORT void dpnp_fill_diagonal_c(void* array1_in, void* val, size_t* shap
  */
 template <typename _DataType_input1, typename _DataType_input2, typename _DataType_output>
 INP_DLLEXPORT Deps* dpnp_floor_divide_c(void* result_out,
-                                       const void* input1_in,
-                                       const size_t input1_size,
-                                       const size_t* input1_shape,
-                                       const size_t input1_shape_ndim,
-                                       const void* input2_in,
-                                       const size_t input2_size,
-                                       const size_t* input2_shape,
-                                       const size_t input2_shape_ndim,
-                                       const size_t* where,
-                                       Deps* deps);
-
+                                        const void* input1_in,
+                                        const size_t input1_size,
+                                        const size_t* input1_shape,
+                                        const size_t input1_shape_ndim,
+                                        const void* input2_in,
+                                        const size_t input2_size,
+                                        const size_t* input2_shape,
+                                        const size_t input2_shape_ndim,
+                                        const size_t* where,
+                                        Deps* deps);
+ 
 /**
  * @ingroup BACKEND_API
  * @brief floor_divide function.
@@ -993,15 +993,15 @@ INP_DLLEXPORT Deps* dpnp_floor_divide_c(void* result_out,
  */
 template <typename _DataType_input1, typename _DataType_input2, typename _DataType_output>
 INP_DLLEXPORT Deps* dpnp_floor_divide_c(void* result_out,
-                                       const void* input1_in,
-                                       const size_t input1_size,
-                                       const size_t* input1_shape,
-                                       const size_t input1_shape_ndim,
-                                       const void* input2_in,
-                                       const size_t input2_size,
-                                       const size_t* input2_shape,
-                                       const size_t input2_shape_ndim,
-                                       const size_t* where);
+                                        const void* input1_in,
+                                        const size_t input1_size,
+                                        const size_t* input1_shape,
+                                        const size_t input1_shape_ndim,
+                                        const void* input2_in,
+                                        const size_t input2_size,
+                                        const size_t* input2_shape,
+                                        const size_t input2_shape_ndim,
+                                        const size_t* where);
 /**
  * @ingroup BACKEND_API
  * @brief modf function.
@@ -1054,17 +1054,17 @@ INP_DLLEXPORT void dpnp_ones_like_c(void* result, size_t size);
  */
 template <typename _DataType_output, typename _DataType_input1, typename _DataType_input2>
 INP_DLLEXPORT Deps* dpnp_remainder_c(void* result_out,
-                                    const void* input1_in,
-                                    const size_t input1_size,
-                                    const size_t* input1_shape,
-                                    const size_t input1_shape_ndim,
-                                    const void* input2_in,
-                                    const size_t input2_size,
-                                    const size_t* input2_shape,
-                                    const size_t input2_shape_ndim,
-                                    const size_t* where,
-                                    Deps* deps);
-
+                                     const void* input1_in,
+                                     const size_t input1_size,
+                                     const size_t* input1_shape,
+                                     const size_t input1_shape_ndim,
+                                     const void* input2_in,
+                                     const size_t input2_size,
+                                     const size_t* input2_shape,
+                                     const size_t input2_shape_ndim,
+                                     const size_t* where,
+                                     Deps* deps);
+ 
 /**
  * @ingroup BACKEND_API
  * @brief remainder function.
@@ -1084,16 +1084,16 @@ INP_DLLEXPORT Deps* dpnp_remainder_c(void* result_out,
  */
 template <typename _DataType_output, typename _DataType_input1, typename _DataType_input2>
 INP_DLLEXPORT Deps* dpnp_remainder_c(void* result_out,
-                                    const void* input1_in,
-                                    const size_t input1_size,
-                                    const size_t* input1_shape,
-                                    const size_t input1_shape_ndim,
-                                    const void* input2_in,
-                                    const size_t input2_size,
-                                    const size_t* input2_shape,
-                                    const size_t input2_shape_ndim,
-                                    const size_t* where);
-/**
+                                     const void* input1_in,
+                                     const size_t input1_size,
+                                     const size_t* input1_shape,
+                                     const size_t input1_shape_ndim,
+                                     const void* input2_in,
+                                     const size_t input2_size,
+                                     const size_t* input2_shape,
+                                     const size_t input2_shape_ndim,
+                                     const size_t* where);
+/** 
  * @ingroup BACKEND_API
  * @brief repeat elements of an array.
  *
