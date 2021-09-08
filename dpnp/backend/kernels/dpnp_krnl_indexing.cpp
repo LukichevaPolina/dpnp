c5ab440fddd14bde07d9e15e090a6c0f122c28e5
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-09-08 05:41:34 -0500

    Add checking parametrs in dpnp_choose_c

--- dpnp/backend/kernels/dpnp_krnl_indexing.cpp
+++ dpnp/backend/kernels/dpnp_krnl_indexing.cpp
@@ -39,6 +39,10 @@ class dpnp_choose_c_kernel;
 template <typename _DataType1, typename _DataType2>
 void dpnp_choose_c(void* result1, void* array1_in, std::vector<void*> choices1, size_t size)
 {
+    if ((array1_in == nullptr) || (result1 == nullptr) || !size)
+    {
+        return;
+    }
     DPNPC_ptr_adapter<_DataType1> input1_ptr(array1_in, size);
     _DataType1* array_in = input1_ptr.get_ptr();
 
