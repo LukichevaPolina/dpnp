12189f8a82f07b5900a6f44bdc2f78a57b98fe7b
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-09-09 05:05:50 -0500

    Fixies in choose

--- dpnp/dpnp_iface_indexing.py
+++ dpnp/dpnp_iface_indexing.py
@@ -82,11 +82,13 @@ def choose(x1, choices, out=None, mode='raise'):
     :obj:`take_along_axis` : Preferable if choices is an array.
     """
     x1_desc = dpnp.get_dpnp_descriptor(x1)
-
+    
     choices_list =[]
     for choice in choices:
         choices_list.append(dpnp.get_dpnp_descriptor(choice))
 
+    size = x1_desc.size
+    choices_size = choices_list[0].size
     if x1_desc:
         if any(not desc for desc in choices_list):
             pass
@@ -97,8 +99,6 @@ def choose(x1, choices, out=None, mode='raise'):
         elif any(not choices[0].dtype == choice.dtype for choice in choices):
             pass
         else:
-            size = x1_desc.size
-            choices_size = choices_list[0].size
             if any(choice.size != choices_size or choice.size != size for choice in choices):
                 pass
             elif any(x >= choices_size for x in dpnp.asnumpy(x1)):
